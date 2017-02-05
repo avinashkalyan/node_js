6448c05e5d8346d613015b985ab72b5c78d81d50
Avinash <Avinash>
2017-02-05 11:30:40 +0530

    initial commit

---
+++ console_methods.js
@@ -0,0 +1,8 @@
+const name1='Avinash';
+
+console.log('Hello%s',2);//in this case no space between hello and 2.
+console.log('Hello',2);//in this case space between hello and 2.
+debugger; //to see effect run like node debug console_methods.js
+console.warn(`my name is ${name1}`);//used template string with `` and ${}
+
+console.error(new Error('This is an error'));
